36701a5448e149f031592b907dace2ec144e6c65
LuoYang <toluoyang@qq.com>
2019-07-03 16:25:50 +0800

    修改 RadiusButton

--- app/src/main/java/com/luoyang/quickrun/ui/widget/RadiusButton.kt
+++ app/src/main/java/com/luoyang/quickrun/ui/widget/RadiusButton.kt
@@ -6,6 +6,7 @@ import android.util.AttributeSet
 import android.view.Gravity
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.ColorInt
 import androidx.cardview.widget.CardView
 import com.luoyang.quickrun.R
 
@@ -17,28 +18,52 @@ class RadiusButton(context: Context, attrs: AttributeSet, defStyLeAttr: Int) :
     var textView: TextView = TextView(context)
         private set
 
+    @ColorInt
+    var mCardBackgroundColor = resources.getColor(android.R.color.white)
+    @ColorInt
+    var mTextColor = resources.getColor(R.color.mtrl_chip_text_color)
+    var mCardElevation = 0F
+
     init {
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.RadiusButton)
         if (attributes != null) {
             textView.text = attributes.getString(R.styleable.RadiusButton_text) ?: "Radius Button"
-            textView.setTextColor(attributes.getColor(R.styleable.RadiusButton_textColor, textView.currentTextColor))
+            mTextColor = attributes.getColor(R.styleable.RadiusButton_textColor, textView.currentTextColor)
+            textView.setTextColor(mTextColor)
             isEnabled = attributes.getBoolean(R.styleable.RadiusButton_enabled, true)
-            if (isEnabled) {
-                setCardBackgroundColor(cardBackgroundColor)
-                textView.setTextColor(textView.currentTextColor)
-                cardElevation = 4F
-            } else {
-                setCardBackgroundColor(Color.parseColor("#FFD6D7D7"))
-                textView.setTextColor(Color.parseColor("#FFA0A0A0"))
-                cardElevation = 0F
-            }
         }
         attributes.recycle()
+
+        val attributes1 = context.obtainStyledAttributes(attrs, R.styleable.CardView)
+        if (attributes1 != null) {
+            mCardBackgroundColor = attributes1.getColor(
+                R.styleable.CardView_cardBackgroundColor,
+                resources.getColor(android.R.color.white)
+            )
+            mCardElevation = attributes1.getDimension(R.styleable.CardView_cardElevation, 0F)
+        }
+        attributes1.recycle()
+
         val layoutParams = LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         layoutParams.gravity = Gravity.CENTER
+        layoutParams.leftMargin = 16
+        layoutParams.rightMargin = 16
         layoutParams.topMargin = 40
         layoutParams.bottomMargin = 40
         addView(textView, layoutParams)
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+        if (isEnabled) {
+            setCardBackgroundColor(mCardBackgroundColor)
+            textView.setTextColor(mTextColor)
+            cardElevation = mCardElevation
+        } else {
+            setCardBackgroundColor(Color.parseColor("#FFD6D7D7"))
+            textView.setTextColor(Color.parseColor("#FFA0A0A0"))
+            cardElevation = 0F
+        }
     }
 
 }
