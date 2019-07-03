901956d48f8d7ff05261c40a19989b359f908b14
LuoYang <toluoyang@qq.com>
2019-07-03 16:25:50 +0800

    修改 RadiusButton

--- app/src/main/java/com/luoyang/quickrun/ui/widget/RadiusButton.kt
+++ app/src/main/java/com/luoyang/quickrun/ui/widget/RadiusButton.kt
@@ -3,6 +3,7 @@ package com.luoyang.quickrun.ui.widget
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.Gravity
 import android.view.ViewGroup
 import android.widget.TextView
@@ -19,26 +20,31 @@ class RadiusButton(context: Context, attrs: AttributeSet, defStyLeAttr: Int) :
 
     init {
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.RadiusButton)
-        if (attributes != null) {
-            textView.text = attributes.getString(R.styleable.RadiusButton_text) ?: "Radius Button"
-            textView.setTextColor(attributes.getColor(R.styleable.RadiusButton_textColor, textView.currentTextColor))
-            isEnabled = attributes.getBoolean(R.styleable.RadiusButton_enabled, true)
-            if (isEnabled) {
-                setCardBackgroundColor(cardBackgroundColor)
-                textView.setTextColor(textView.currentTextColor)
-                cardElevation = 4F
-            } else {
-                setCardBackgroundColor(Color.parseColor("#FFD6D7D7"))
-                textView.setTextColor(Color.parseColor("#FFA0A0A0"))
-                cardElevation = 0F
-            }
-        }
+        textView.text = attributes.getString(R.styleable.RadiusButton_text) ?: "Radius Button"
+        textView.setTextColor(
+            attributes.getColor(
+                R.styleable.RadiusButton_textColor, Color.parseColor("#54000000")
+            )
+        )
+        textView.setTextSize(
+            TypedValue.COMPLEX_UNIT_SP,
+            attributes.getDimension(R.styleable.RadiusButton_textSize, 14F)
+        )
+        isEnabled = attributes.getBoolean(R.styleable.RadiusButton_enabled, true)
         attributes.recycle()
+        textView.gravity = Gravity.CENTER
+
         val layoutParams = LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         layoutParams.gravity = Gravity.CENTER
-        layoutParams.topMargin = 40
-        layoutParams.bottomMargin = 40
+        layoutParams.leftMargin = pix2dip(8)
+        layoutParams.rightMargin = pix2dip(8)
+        layoutParams.topMargin = pix2dip(16)
+        layoutParams.bottomMargin = pix2dip(16)
         addView(textView, layoutParams)
     }
 
+    private fun pix2dip(pxValue: Int): Int {
+        val scale = context.resources.getDisplayMetrics().density
+        return (pxValue / scale + 0.5f).toInt()
+    }
 }
